462839e5df736028400bf7e86a48255bde6ea6ec
10Sadique <jafarsjahan@gmail.com>
2023-05-16 15:46:09 +0600

    fetch posts by user endpoint added

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,20 @@
+import Prompt from "@models/prompt";
+import { connectDB } from "@utils/database";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectDB();
+
+        const prompts = await Prompt.find({
+            creator: params.id,
+        }).populate("creator");
+
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+        });
+    } catch (error) {
+        return new Response("Falied to find prompts.", {
+            status: 500,
+        });
+    }
+};
