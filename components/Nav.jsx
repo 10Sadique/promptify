642c2d84b5c7ddacaa9880a7146aac78c4adabd7
10Sadique <jafarsjahan@gmail.com>
2023-05-16 10:41:05 +0600

    auth session added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,22 +3,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
-import { signIn, signOut, getSession, getProviders } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         };
 
-        setProviders();
+        setUpProviders();
     }, []);
 
     return (
@@ -36,7 +36,7 @@ const Nav = () => {
 
             {/* Desktop Navigation */}
             <div className="hidden sm:flex">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href={"/create-prompt"} className="black_btn">
                             Create Prompt
@@ -52,7 +52,7 @@ const Nav = () => {
 
                         <Link href={"/profile"}>
                             <Image
-                                src={`/assets/images/logo.svg`}
+                                src={session?.user?.image}
                                 width={37}
                                 height={37}
                                 className="rounded-full"
@@ -79,10 +79,10 @@ const Nav = () => {
 
             {/* Mobile Navigation */}
             <div className="relative flex sm:hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
-                            src={`/assets/images/logo.svg`}
+                            src={session?.user?.image}
                             width={37}
                             height={37}
                             className="rounded-full"
